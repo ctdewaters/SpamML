bf7307c47f9901002d75baa4901998dcc7703ec0
Collin DeWaters <ctdewaters@icloud.com>
2020-10-23 15:05:39 -0400

    Initial Commit

---
+++ Shared/SpamMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpamMLApp.swift
+//  Shared
+//
+//  Created by Collin DeWaters on 10/23/20.
+//
+
+import SwiftUI
+
+@main
+struct SpamMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
